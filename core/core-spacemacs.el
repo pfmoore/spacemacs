a25b9f41900d6f1295c2c8177b38c06c49e9ff1b
Paul Moore <p.f.moore@gmail.com>
2016-06-10 14:46:59 +0100

    Tidy up for making a PR

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -58,12 +58,6 @@
   "Whether or not spacemacs has finished initializing by completing
 the final step of executing code in `emacs-startup-hook'.")
 
-;; TODO: Use of focus-in-hook means first frame starts with the wrong font,
-;; and correcting it happens after a definite delay.
-;; TODO: Investigate errors in messages:
-;;    Error during redisplay: (eval (spaceline-ml-main)) signaled
-;;          (wrong-type-argument hash-table-p nil
-;;    <t> is undefined
 (defun spacemacs/set-font (frame)
   (select-frame frame)
   (when (font-family-list)
@@ -113,21 +107,6 @@ the final step of executing code in `emacs-startup-hook'.")
     (setq-default spacemacs--cycle-themes (cdr dotspacemacs-themes)))
   ;; font
   (add-hook 'after-make-frame-functions #'spacemacs/set-font)
-  ;(spacemacs|do-after-display-system-init
-  ; ;; If you are thinking to remove this call to `message', think twice. You'll
-  ; ;; break the life of several Spacemacser using Emacs in daemon mode. Without
-  ; ;; this, their chosen font will not be set on the *first* instance of
-  ; ;; emacsclient, at least if different than their system font. You don't
-  ; ;; believe me? Go ahead, try it. After you'll have notice that this was true,
-  ; ;; increase the counter bellow so next people will give it more confidence.
-  ; ;; Counter = 1
-  ; (message "Setting the font...")
-  ; (unless (spacemacs/set-default-font dotspacemacs-default-font)
-  ;   (spacemacs-buffer/warning
-  ;    "Cannot find any of the specified fonts (%s)! Font settings may not be correct."
-  ;    (if (listp (car dotspacemacs-default-font))
-  ;        (mapconcat 'car dotspacemacs-default-font ", ")
-  ;      (car dotspacemacs-default-font)))))
   ;; spacemacs init
   (setq inhibit-startup-screen t)
   (spacemacs-buffer/goto-buffer)
